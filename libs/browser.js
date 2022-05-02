a264135e8c98747a0f8004e19a7d503f3fe4c858
algo7 <11154774+algo7@users.noreply.github.com>
2022-05-02 17:58:04 +0200

    change browser headless to true

--- libs/browser.js
+++ libs/browser.js
@@ -19,7 +19,7 @@ class Browser {
     constructor() {
         // Puppeteer configs
         this.config = {
-            headless: false,
+            headless: true,
             devtools: false,
             defaultViewport: {
                 width: 1280,
