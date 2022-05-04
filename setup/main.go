1371bbf14e50f5aa5d8666a9d0c6e2ea35893f94
algo7 <11154774+algo7@users.noreply.github.com>
2022-05-04 22:45:52 +0200

    refactoring + err msg change

--- setup/main.go
+++ setup/main.go
@@ -4,25 +4,51 @@ package main
 import (
 	"errors"
 	"fmt"
+	"log"
 	"os"
 
 	git "github.com/go-git/go-git/v5"
 )
 
-var directoryCreationFailure = errors.New("Failed to create directory")
-var getDirectoryFailure = errors.New("Failed to get the current directory")
+var (
+	errDirectoryCreation = errors.New("FAILED TO CREATE DIRECTORY")
+	errGetDirectory      = errors.New("FAILED TO GET THE CURRENT DIRECTORY")
+	errCloneRepo         = errors.New("FAILED TO CLONE THE REPOSITORY")
+)
 
 func main() {
 
 	// Get the current directory
 	currentDir, err := getCurrentDir()
 
+	// Check for errors
 	if err != nil {
-		panic(err)
+		log.Panic(err)
 	}
 
+	// Print the current directory
+	fmt.Println("Current directory: ", currentDir)
+
+	// Create the directory
+	dirName, err := createDirectory("Root")
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	// Print the message
+	fmt.Println("Directory created:", currentDir+"/"+dirName)
+	dirFullPath := currentDir + "/" + dirName
+
 	// Call the clone repo function
-	cloneRepo(currentDir)
+	msg, err := cloneRepo(dirFullPath)
+
+	// Check for errors
+	if err != nil {
+		log.Panic(err)
+	}
+
+	fmt.Println(msg)
 }
 
 // The function to get the current working directory
@@ -31,23 +57,36 @@ func getCurrentDir() (string, error) {
 	pwd, err := os.Getwd()
 	// Check for errors
 	if err != nil {
-		return "Failed", getDirectoryFailure
+		return "Failed", errGetDirectory
 	}
-	// Print the current directory
-	fmt.Println("Current directory:", pwd)
 
 	return pwd, err
 }
 
-func cloneRepo(path string) {
-	r, err := git.PlainClone(path, false, &git.CloneOptions{
+// The function to create a directory
+func createDirectory(name string) (string, error) {
+
+	// Create the directory
+	err := os.Mkdir(name, os.ModePerm)
+
+	// Check for errors
+	if err != nil {
+		return "Failed to Create Directory", errDirectoryCreation
+	}
+	return name, nil
+}
+
+func cloneRepo(path string) (string, error) {
+	_, err := git.PlainClone(path, false, &git.CloneOptions{
 		URL:        "https://github.com/algo7/TripAdvisor-Review-Scraper.git",
 		RemoteName: "origin",
 		Progress:   os.Stdout,
 	})
 
 	if err != nil {
-		fmt.Println(err)
+		return "Failed", errCloneRepo
 	}
+
+	return "Repo cloned", nil
 
 }
