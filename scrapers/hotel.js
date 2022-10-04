7731cf705a22e1fd03562afa43f1eddabee0fd25
algo7 <11154774+algo7@users.noreply.github.com>
2022-10-05 00:00:05 +0200

    Fix unable to get comment titles due to TripAdivsor changing class name

--- scrapers/hotel.js
+++ scrapers/hotel.js
@@ -146,7 +146,7 @@ const scrape = async (totalReviewCount, reviewPageUrls, position, hotelName, hot
             const commentTitle = await page.evaluate(async () => {
 
                 // Extract a tags
-                const commentTitleBlocks = document.getElementsByClassName('fCitC');
+                const commentTitleBlocks = document.getElementsByClassName('Qwuub')
 
                 // Array to store the comment titles
                 const titles = [];
