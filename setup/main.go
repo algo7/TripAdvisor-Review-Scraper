db27d9726cbd946b278643cbcbbe5f69aa92795c
algo7 <11154774+algo7@users.noreply.github.com>
2022-11-23 06:38:47 +0100

    add review language option to the setup script and the docker-compose files

--- setup/main.go
+++ setup/main.go
@@ -30,6 +30,7 @@ var (
 	errMissingSourceFiles       = errors.New("MISSING SOURCE FILES")
 	errInputScrapMode           = errors.New("INVALID SCRAP MODE")
 	errInputConcurrency         = errors.New("INVALID CONCURRENCY VALUE")
+	errInputLanguage            = errors.New("INVALID CONCURRENCY VALUE")
 	errDockerComposeYmlNotFound = errors.New("DOCKER-COMPSE-PROD.YML NOT FOUND")
 	errValueReplace             = errors.New("FAILED TO REPLACE VALUE")
 )
@@ -157,6 +158,16 @@ func userInputs(path string) error {
 		return errInputScrapMode
 	}
 
+	// Get review language
+	fmt.Println("Enter the language of the reviews (en or fr):")
+	var lang string
+	_, err = fmt.Scanf("%s\n", &lang)
+
+	// Input validation
+	if err != nil || (lang != "en" && mode != "fr") {
+		return errInputLanguage
+	}
+
 	// Get concurrency value
 	fmt.Println("Enter the concurrency value (ex: 10):")
 	var i int
@@ -170,6 +181,7 @@ func userInputs(path string) error {
 	// Print the user output
 	fmt.Println("Scrap mode:", mode)
 	fmt.Println("Concurrency value:", i)
+	fmt.Println("Review language:", lang)
 
 	// Read the docker-compose-prod.yml file
 	dockerComposeFilePath := filepath.Join(path, "Project_Files/docker-compose-prod.yml")
@@ -183,11 +195,15 @@ func userInputs(path string) error {
 	scrapModeRegex := regexp.MustCompile("SCRAPE_MODE:(.*)")
 	// Regex to match the concurrency value
 	concurrencyRegex := regexp.MustCompile("CONCURRENCY:(.*)")
+	// Regex to match the review language option
+	reviewLaguageRegex := regexp.MustCompile("LANGUAGE:(.*)")
 
 	// Replace the scrap mode with the input
 	scrapModeChanged := scrapModeRegex.ReplaceAllString(string(content), "SCRAPE_MODE: "+mode)
 	// Replace the concurrency value with the input
 	concurrencyChanged := concurrencyRegex.ReplaceAllString(scrapModeChanged, "CONCURRENCY: "+strconv.Itoa(i))
+	// Replace the review language with the input
+	reviewLanguageChanged := reviewLaguageRegex.ReplaceAllString(concurrencyChanged, "LANGUAGE: "+lang)
 
 	f, err := os.OpenFile(dockerComposeFilePath, os.O_RDWR|os.O_CREATE|os.O_TRUNC, os.ModePerm)
 
@@ -199,7 +215,7 @@ func userInputs(path string) error {
 	defer f.Close()
 
 	// Write the new content to the file
-	_, err = f.WriteString(concurrencyChanged)
+	_, err = f.WriteString(reviewLanguageChanged)
 
 	if err != nil {
 		return errValueReplace
