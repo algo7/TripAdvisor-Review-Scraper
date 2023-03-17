cd789f8f621568a2d75e2a04d3d4775fd2d9377f
algo7 <11154774+algo7@users.noreply.github.com>
2023-03-17 04:54:53 +0100

    formatting

--- container_provisioner/api/fiber.go
+++ container_provisioner/api/fiber.go
@@ -4,11 +4,13 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-// Custom config
-app := fiber.New(fiber.Config{
-    Prefork:       true,
-    CaseSensitive: true,
-    StrictRouting: true,
-    ServerHeader:  "Algo7 TripAdvisor Scraper",
-    AppName: "Algo7 TripAdvisor Scraper v1.0.0"
-})
+var (
+	// Custom config
+	app = fiber.New(fiber.Config{
+		Prefork:       true,
+		CaseSensitive: true,
+		StrictRouting: true,
+		ServerHeader:  "Algo7 TripAdvisor Scraper",
+		AppName:       "Algo7 TripAdvisor Scraper v1.0.0",
+	})
+)
