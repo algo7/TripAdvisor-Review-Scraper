2cd0c06998c41c4bf77227cd2055f6c66082f4a9
algo7 <11154774+algo7@users.noreply.github.com>
2023-03-17 06:04:02 +0100

    url validation logic in the submission route

--- container_provisioner/api/controllers.go
+++ container_provisioner/api/controllers.go
@@ -1,6 +1,10 @@
 package api
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"container_provisioner/utils"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 // getMain renders the main page
 func getMain(c *fiber.Ctx) error {
@@ -10,6 +14,13 @@ func getMain(c *fiber.Ctx) error {
 // postProvision is the handler for the form submission
 func postProvision(c *fiber.Ctx) error {
 
+	// Get the URL from the form
 	url := c.FormValue("url")
+
+	// Check if the URL matches the regex
+	if !utils.ValidateTripAdvisorHotelURL(url) {
+		return c.SendString("Invalid URL")
+	}
+
 	return c.SendString("Hello, World 👋!" + url)
 }
