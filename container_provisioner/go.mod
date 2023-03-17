b67dd917ad66894cc8128506d6c5c2648263d941
algo7 <11154774+algo7@users.noreply.github.com>
2023-03-17 03:02:15 +0100

    cloudflare r2 logic

--- container_provisioner/go.mod
+++ container_provisioner/go.mod
@@ -2,10 +2,30 @@ module container_provisioner
 
 go 1.19
 
-require github.com/docker/docker v23.0.1+incompatible
+require (
+	github.com/aws/aws-sdk-go-v2 v1.17.6
+	github.com/aws/aws-sdk-go-v2/config v1.18.18
+	github.com/aws/aws-sdk-go-v2/credentials v1.13.17
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.30.6
+	github.com/docker/docker v23.0.1+incompatible
+)
 
 require (
 	github.com/Microsoft/go-winio v0.6.0 // indirect
+	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.4.10 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.13.0 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.30 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.24 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.31 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/v4a v1.0.22 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.9.11 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.1.25 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.24 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.13.24 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.12.5 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.14.5 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.18.6 // indirect
+	github.com/aws/smithy-go v1.13.5 // indirect
 	github.com/docker/distribution v2.8.1+incompatible // indirect
 	github.com/docker/go-connections v0.4.0 // indirect
 	github.com/docker/go-units v0.5.0 // indirect
---
+++ container_provisioner/utils/cloudflare.go
@@ -0,0 +1,76 @@
+package utils
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+var (
+	s3Client = createS3Client()
+)
+
+// R2UploadObject upload an object to R2
+func R2UploadObject() {
+
+	// Read file
+	file, err := os.Open("Reviews.csv")
+	defer file.Close()
+	ErrorHandler(err)
+
+	// Upload an object to R2
+	_, err = s3Client.PutObject(context.TODO(), &s3.PutObjectInput{
+		Bucket: &bucketName,
+		Key:    aws.String("All.csv"),
+		Body:   file,
+	})
+	ErrorHandler(err)
+}
+
+// r2ListObjects List objects in R2
+func r2ListObjects() {
+
+	// List objects in R2
+	listObjectsOutput, err := s3Client.ListObjectsV2(context.TODO(), &s3.ListObjectsV2Input{
+		Bucket: &bucketName,
+	})
+	ErrorHandler(err)
+
+	for _, object := range listObjectsOutput.Contents {
+		obj, _ := json.MarshalIndent(object, "", "\t")
+		fmt.Println(string(obj))
+	}
+}
+
+// resolveEndpoint is a custom endpoint resolver for R2
+func resolveEndpoint(service, region string, options ...interface{}) (aws.Endpoint, error) {
+
+	// Logic from the documentation
+	return aws.Endpoint{
+		URL: fmt.Sprintf("https://%s.r2.cloudflarestorage.com", accountId),
+	}, nil
+
+}
+
+// Create a new S3 client
+func createS3Client() *s3.Client {
+
+	// Logic from the documentation
+	r2Resolver := aws.EndpointResolverWithOptionsFunc(resolveEndpoint)
+
+	cfg, err := config.LoadDefaultConfig(context.TODO(),
+		config.WithEndpointResolverWithOptions(r2Resolver),
+		config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(accessKeyId, accessKeySecret, "")),
+	)
+	ErrorHandler(err)
+
+	client := s3.NewFromConfig(cfg)
+
+	return client
+}
