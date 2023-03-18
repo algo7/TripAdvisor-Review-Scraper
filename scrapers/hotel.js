f24a770e8ab3d68b35e0d9233ed25627de6a1613
algo7 <11154774+algo7@users.noreply.github.com>
2023-03-18 10:29:09 +0100

    convert Pages Left matric in the progress report logic to type string so it doesnt print weird characters when being read using the browser

--- scrapers/hotel.js
+++ scrapers/hotel.js
@@ -147,17 +147,17 @@ const scrape = async (totalReviewCount, reviewPageUrls, position, hotelName, hot
             // Determin current URL
             const currentURL = page.url();
 
+            // Progress Report
             if (!IS_PROVISIONER) {
-                // Progress Report
                 console.log({
                     'Scraping': currentURL,
                     'Pages Left': reviewPageUrls.length - 1 - index,
-                    'Progress': `${Math.round(((index + 1) / reviewPageUrls.length * 100), 1)}% `,
+                    'Progress': `${Math.round(((index + 1) / reviewPageUrls.length * 100), 1)}%`,
                 });
             } else {
-                console.log("Scraping", currentURL)
-                console.log("Pages Left", reviewPageUrls.length - 1 - index)
-                console.log("Progress", `${Math.round(((index + 1) / reviewPageUrls.length * 100), 1)}% `)
+                console.log("Scraping: ", currentURL)
+                console.log("Pages Left: ", `${reviewPageUrls.length - 1 - index}`)
+                console.log("Progress: ", `${Math.round(((index + 1) / reviewPageUrls.length * 100), 1)}%`)
             }
 
 
