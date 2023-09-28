0252a4038d6e01c2132ad0696c89a23004ca16c8
algo7 <11154774+algo7@users.noreply.github.com>
2023-09-28 20:14:58 +0200

    remove autopull

--- container_provisioner/main.go
+++ container_provisioner/main.go
@@ -5,7 +5,6 @@ import (
 	"os"
 
 	"github.com/algo7/TripAdvisor-Review-Scraper/container_provisioner/api"
-	"github.com/algo7/TripAdvisor-Review-Scraper/container_provisioner/containers"
 	"github.com/algo7/TripAdvisor-Review-Scraper/container_provisioner/database"
 )
 
@@ -15,9 +14,6 @@ func main() {
 	if os.Getenv("R2_URL") == "" {
 		log.Fatal("R2_URL environment variable not set")
 	}
-
-	// Pull / update the scraper image
-	containers.PullImage("ghcr.io/algo7/tripadvisor-review-scraper/scraper:latest")
 
 	// Check if the redis server is up and running
 	database.RedisConnectionCheck()
