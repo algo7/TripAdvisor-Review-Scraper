e00a86cd93ca0c26aec463b4cf7767af53300499
algo7 <11154774+algo7@users.noreply.github.com>
2023-10-04 05:46:33 +0200

    add scrape function template to airline.js

--- scraper/scrapers/airline.js
+++ scraper/scrapers/airline.js
@@ -110,3 +110,167 @@ const extractAllReviewPageUrls = async (hotelUrl, position, browser) => {
         throw err;
     }
 };
+
+/**
+ * Scrape the page
+ * @param {Number} totalReviewCount - The total review count
+ * @param {Array<String>} reviewPageUrls - The review page urls
+ * @param {Number} [position] - The index of the hotel page in the list
+ * @param {String} airlineName - The name of the hotel
+ * @param {String} [hotelId] - The id of the hotel
+ * @param {Object} browser - A browser instance
+ * @returns {Promise<Object| Error>} - THe final data
+ */
+const scrape = async (totalReviewCount, reviewPageUrls, position, hotelName, hotelId, browser) => {
+    try {
+
+        // Open a new page
+        const page = await browser.getNewPage()
+
+        // Array to hold the review info
+        const allReviews = [];
+
+        for (let index = 0; index < reviewPageUrls.length; index++) {
+
+            // Navigate to the page below
+            await page.goto(reviewPageUrls[index], { waitUntil: 'networkidle2', });
+
+            // Wait for the content to load
+            await page.waitForSelector('body');
+
+            // Determin current URL
+            const currentURL = page.url();
+
+            // Progress Report
+            if (!IS_PROVISIONER) {
+                console.log({
+                    'Scraping': currentURL,
+                    'Pages Left': reviewPageUrls.length - 1 - index,
+                    'Progress': `${Math.round(((index + 1) / reviewPageUrls.length * 100), 1)}%`,
+                });
+            } else {
+                console.log("Scraping: ", currentURL)
+                console.log("Pages Left: ", `${reviewPageUrls.length - 1 - index}`)
+                console.log("Progress: ", `${Math.round(((index + 1) / reviewPageUrls.length * 100), 1)}%`)
+            }
+
+
+            // In browser code
+            // Extract comments title
+            const commentTitle = await page.evaluate(async () => {
+
+                // Extract a tags
+                const commentTitleBlocks = document.getElementsByClassName('Qwuub')
+
+                // Array to store the comment titles
+                const titles = [];
+
+                // Higher order functions don't work in the browser
+                for (let index = 0; index < commentTitleBlocks.length; index++) {
+                    titles.push(commentTitleBlocks[index].children[0].innerText);
+                }
+
+                return titles;
+            });
+
+            // Extract comment rating
+            const commentRating = await page.evaluate(async () => {
+                const commentRatingBlocks = document.getElementsByClassName("Hlmiy")
+                const ratings = [];
+
+                for (let index = 0; index < commentRatingBlocks.length; index++) {
+                    ratings.push(commentRatingBlocks[index].children[0].classList[1]);
+                }
+
+                return ratings;
+            });
+
+            // Extract date of stay
+            const commentDateOfReview = await page.evaluate(async () => {
+
+                // const commentDateOfStayBlocks = document.getElementsByClassName('teHYY')
+                const commentDateBlocks = document.getElementsByClassName("cRVSd")
+
+                // const datesOfStay = [];
+                const datesOfReview = [];
+
+
+                // for (let index = 0; index < commentDateOfStayBlocks.length; index++) {
+
+                //     // Split the date of stay text block into an array
+                //     const splitted = commentDateOfStayBlocks[index].innerText.split(' ')
+
+                //     datesOfStay.push({
+                //         month: splitted[3],
+                //         year: splitted[4],
+                //     });
+                // }
+
+                for (let index = 0; index < commentDateBlocks.length; index++) {
+
+                    // Split the date of comment text block into an array
+                    const splitted = commentDateBlocks[index].children[0].innerText.split('review').pop().split(' ')
+
+                    datesOfReview.push({
+                        month: splitted[1],
+                        year: splitted[2],
+                    });
+                }
+
+                return datesOfReview;
+            });
+
+            // Extract comments text
+            const commentContent = await page.evaluate(async () => {
+
+                const commentContentBlocks = document.getElementsByClassName('QewHA H4 _a');
+
+                // Array use to store the comments
+                const comments = [];
+
+                for (let index = 0; index < commentContentBlocks.length; index++) {
+                    comments.push(commentContentBlocks[index].children[0].innerText);
+                }
+
+                return comments;
+            });
+
+            // Format (for CSV processing) the reviews so each review of each page is in an object
+            const formatted = commentContent.map((comment, index) => {
+                return {
+                    title: commentTitle[index],
+                    content: comment,
+                    month: monthStringToNumber(commentDateOfReview[index].month),
+                    year: commentDateOfReview[index].year,
+                    rating: commentRatingStringToNumber(commentRating[index]),
+                };
+            });
+
+            // Push the formmated review to the  array
+            allReviews.push(formatted);
+
+        }
+
+        // Hand back the page so it's available again
+        browser.handBack(page);
+
+        // Convert 2D array to 1D
+        const reviewFlattened = allReviews.flat();
+
+        // Data structure to be written to file
+        const finalData = {
+            hotelName,
+            hotelId,
+            count: totalReviewCount,
+            actualCount: reviewFlattened.length,
+            position,
+            allReviews: reviewFlattened,
+            fileName: `${position}_${reviewPageUrls[0].split('-')[4]}`,
+        };
+
+        return finalData;
+
+    } catch (err) {
+        throw err;
+    }
+};
