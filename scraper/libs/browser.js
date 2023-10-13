682d7a43968417d283975a7706046edc5c64b60c
algo7 <11154774+algo7@users.noreply.github.com>
2023-10-13 20:44:57 +0200

    refactor scraper logic

--- scraper/libs/browser.js
+++ scraper/libs/browser.js
@@ -60,7 +60,7 @@ const browserArgs = [
 ]
 
 // If proxy is enabled
-if (PROXY_ADDRESS) browserArgs.push(`--proxy-server=${PROXY_ENABLED}`)
+if (PROXY_ADDRESS) browserArgs.push(`--proxy-server=${PROXY_ADDRESS}`)
 
 /**
  * Create a browser instance
