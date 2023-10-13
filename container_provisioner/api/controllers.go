1a770f122c4b32b79cd0f18054ef092e44715444
algo7 <11154774+algo7@users.noreply.github.com>
2023-10-13 10:19:05 +0200

    submission page msg formatting

--- container_provisioner/api/controllers.go
+++ container_provisioner/api/controllers.go
@@ -161,7 +161,7 @@ func postProvision(c *fiber.Ctx) error {
 	return c.Render("submission", fiber.Map{
 		"Title": "Algo7 TripAdvisor Scraper",
 		// "Message": fmt.Sprintf("Your request has been submitted. You will receive an email at %s when the data is ready", email),
-		"Message1":    "Your request has been submitted. ",
+		"Message1":    "Your request has been submitted",
 		"Message2":    "You can check the progress of your request below",
 		"Message3":    "Once it's done, you can return to the main page to download the data",
 		"ContainerId": containerID,
