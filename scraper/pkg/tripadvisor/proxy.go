af86a8428d108a394a46b6c225e823e0ad487e22
algo7 <11154774+algo7@users.noreply.github.com>
2024-01-23 09:29:58 +0100

    Refactor GetHTTPClientWithProxy to use proxy URL instead of custom dialer

--- scraper/pkg/tripadvisor/proxy.go
+++ scraper/pkg/tripadvisor/proxy.go
@@ -1,36 +1,30 @@
 package tripadvisor
 
 import (
-	"context"
 	"fmt"
 	"log"
 	"net"
 	"net/http"
+	"net/url"
 	"time"
-
-	"golang.org/x/net/proxy"
 )
 
 // GetHTTPClientWithProxy returns an HTTP client that uses the proxy server
 func GetHTTPClientWithProxy(proxyHost string) (*http.Client, error) {
 
+	proxyURL, err := url.Parse(proxyHost)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing proxy URL: %w", err)
+	}
+
 	// Check if the proxy server is operational
-	if !CheckProxyConnection(proxyHost, 5*time.Second) {
+	if !CheckProxyConnection(proxyURL.Host, 5*time.Second) {
 		return nil, fmt.Errorf("proxy server is not operational")
 	}
 	log.Println("Proxy server is operational")
 
-	// Create a SOCKS5 dialer
-	dialer, err := proxy.SOCKS5("tcp", proxyHost, nil, proxy.Direct)
-	if err != nil {
-		return nil, fmt.Errorf("error creating dialer: %w", err)
-	}
-
-	// Configure the HTTP client to use the dialer
 	httpTransport := &http.Transport{
-		DialContext: func(ctx context.Context, network, addr string) (net.Conn, error) {
-			return dialer.Dial(network, addr)
-		},
+		Proxy: http.ProxyURL(proxyURL),
 	}
 
 	return &http.Client{
