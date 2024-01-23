d9c086cf412d8ca85e0be1ff925b02c4374d278d
algo7 <11154774+algo7@users.noreply.github.com>
2024-01-23 06:23:20 +0100

    Add GetHTTPClientWithProxy function to proxy package

--- scraper/pkg/proxy/proxy.go
+++ scraper/pkg/proxy/proxy.go
@@ -1,1 +1,27 @@
 package proxy
+
+import (
+	"fmt"
+	"net/http"
+
+	"golang.org/x/net/proxy"
+)
+
+// GetHTTPClientWithProxy returns an HTTP client that uses the proxy server
+func GetHTTPClientWithProxy(proxyHost string) (*http.Client, error) {
+	// Create a SOCKS5 dialer
+	dialer, err := proxy.SOCKS5("tcp", proxyHost, nil, proxy.Direct)
+	if err != nil {
+		return nil, fmt.Errorf("error creating dialer: %w", err)
+	}
+
+	// Configure the HTTP client to use the dialer
+	httpTransport := &http.Transport{
+		// Set the Dial function to the dialer.Dial method
+		Dial: dialer.Dial,
+	}
+
+	return &http.Client{
+		Transport: httpTransport,
+	}, nil
+}
