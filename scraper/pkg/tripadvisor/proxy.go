b26b189ef84b19212410781cd2b8bf0b6523af7a
algo7 <11154774+algo7@users.noreply.github.com>
2024-01-23 07:38:57 +0100

    Add proxy server operational check

--- scraper/pkg/tripadvisor/proxy.go
+++ scraper/pkg/tripadvisor/proxy.go
@@ -3,14 +3,23 @@ package tripadvisor
 import (
 	"context"
 	"fmt"
+	"log"
 	"net"
 	"net/http"
+	"time"
 
 	"golang.org/x/net/proxy"
 )
 
 // GetHTTPClientWithProxy returns an HTTP client that uses the proxy server
 func GetHTTPClientWithProxy(proxyHost string) (*http.Client, error) {
+
+	// Check if the proxy server is operational
+	if !CheckProxyConnection(proxyHost, 5*time.Second) {
+		return nil, fmt.Errorf("proxy server is not operational")
+	}
+	log.Println("Proxy server is operational")
+
 	// Create a SOCKS5 dialer
 	dialer, err := proxy.SOCKS5("tcp", proxyHost, nil, proxy.Direct)
 	if err != nil {
@@ -27,4 +36,16 @@ func GetHTTPClientWithProxy(proxyHost string) (*http.Client, error) {
 	return &http.Client{
 		Transport: httpTransport,
 	}, nil
+}
+
+// CheckProxyConnection attempts to establish a TCP connection to the proxy server
+// to determine if it is operational.
+func CheckProxyConnection(proxyHost string, timeout time.Duration) bool {
+	conn, err := net.DialTimeout("tcp", proxyHost, timeout)
+	if err != nil {
+		fmt.Printf("Error connecting to proxy: %v\n", err)
+		return false
+	}
+	defer conn.Close()
+	return true
 }
