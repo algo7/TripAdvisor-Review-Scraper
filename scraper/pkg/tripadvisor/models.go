bce55e3f859272e83e2ec45cb307cf75dfcb64f4
algo7 <11154774+algo7@users.noreply.github.com>
2024-01-23 03:23:14 +0100

    Refactor TripAdvisor scraper code

---
+++ scraper/pkg/tripadvisor/models.go
@@ -0,0 +1,64 @@
+package tripadvisor
+
+// Filter is a struct that represents the filter object in the request body to TripAdvisor endpoints.
+type Filter struct {
+	Axis       string   `json:"axis"`
+	Selections []string `json:"selections"`
+}
+
+// Filters is a slice of Filter structs.
+type Filters []Filter
+
+// Variables is a struct that represents the variables object in the request body to TripAdvisor endpoints.
+type Variables struct {
+	LocationID   int     `json:"locationId"`
+	Offset       int     `json:"offset"`
+	Prefs        any     `json:"prefs"` // null
+	Filters      Filters `json:"filters"`
+	InitialPrefs struct {
+	} `json:"initialPrefs"` // empty struct
+	Limit          int    `json:"limit"`
+	FilterCacheKey any    `json:"filterCacheKey"` // null
+	PrefsCacheKey  string `json:"prefsCacheKey"`  // format "locationReviewPrefs_<localtionID>"
+	NeedKeywords   bool   `json:"needKeywords"`   // false
+	KeywordVariant string `json:"keywordVariant"`
+}
+
+// Extensions is a struct that represents the extensions object in the request body to TripAdvisor endpoints.
+type Extensions struct {
+	PreRegisteredQueryID string `json:"preRegisteredQueryId"`
+}
+
+// Request is a struct that represents the request body to query TripAdvisor endpoints.
+type Request struct {
+	Variables  Variables  `json:"variables"`
+	Extensions Extensions `json:"extensions"`
+}
+
+// Requests is a slice of Request structs.
+type Requests []Request
+
+// Response is a struct that represents the response body from TripAdvisor endpoints.
+type Response []struct {
+	Data struct {
+		Locations []struct {
+			ReviewListPage struct {
+				TotalCount int `json:"totalCount"`
+				Reviews    []struct {
+					CreatedDate     string `json:"createdDate"`
+					PublishedDate   string `json:"publishedDate"`
+					Rating          int    `json:"rating"`
+					PublishPlatform string `json:"publishPlatform"`
+					TripInfo        struct {
+						StayDate string `json:"stayDate"`
+						TripType string `json:"tripType"`
+					} `json:"tripInfo"`
+					LocationID int      `json:"locationId"`
+					Labels     []string `json:"labels"`
+					Title      string   `json:"title"`
+					Text       string   `json:"text"`
+				} `json:"reviews"`
+			} `json:"reviewListPage"`
+		} `json:"locations"`
+	} `json:"data,omitempty"`
+}
--- scraper/pkg/tripadvisor/tripadvisor.go
+++ scraper/pkg/tripadvisor/tripadvisor.go
@@ -14,64 +14,6 @@ import (
 // endPointURL is the URL to the TripAdvisor GraphQL endpoint.
 const endPointURL = "https://www.tripadvisor.com/data/graphql/ids"
 
-type Filter struct {
-	Axis       string   `json:"axis"`
-	Selections []string `json:"selections"`
-}
-
-type Filters []Filter
-
-type Variables struct {
-	LocationID   int     `json:"locationId"`
-	Offset       int     `json:"offset"`
-	Prefs        any     `json:"prefs"` // null
-	Filters      Filters `json:"filters"`
-	InitialPrefs struct {
-	} `json:"initialPrefs"` // empty struct
-	Limit          int    `json:"limit"`
-	FilterCacheKey any    `json:"filterCacheKey"` // null
-	PrefsCacheKey  string `json:"prefsCacheKey"`  // format "locationReviewPrefs_<localtionID>"
-	NeedKeywords   bool   `json:"needKeywords"`   // false
-	KeywordVariant string `json:"keywordVariant"`
-}
-
-type Extensions struct {
-	PreRegisteredQueryID string `json:"preRegisteredQueryId"`
-}
-
-// Request is a struct that represents the request body to query TripAdvisor endpoints.
-type Request struct {
-	Variables  Variables  `json:"variables"`
-	Extensions Extensions `json:"extensions"`
-}
-
-type Requests []Request
-
-// Response is a struct that represents the response body from TripAdvisor endpoints.
-type Response []struct {
-	Data struct {
-		Locations []struct {
-			ReviewListPage struct {
-				TotalCount int `json:"totalCount"`
-				Reviews    []struct {
-					CreatedDate     string `json:"createdDate"`
-					PublishedDate   string `json:"publishedDate"`
-					Rating          int    `json:"rating"`
-					PublishPlatform string `json:"publishPlatform"`
-					TripInfo        struct {
-						StayDate string `json:"stayDate"`
-						TripType string `json:"tripType"`
-					} `json:"tripInfo"`
-					LocationID int      `json:"locationId"`
-					Labels     []string `json:"labels"`
-					Title      string   `json:"title"`
-					Text       string   `json:"text"`
-				} `json:"reviews"`
-			} `json:"reviewListPage"`
-		} `json:"locations"`
-	} `json:"data,omitempty"`
-}
-
 // Query is a function that sends a POST request to the TripAdvisor GraphQL endpoint.
 func Query() {
 
